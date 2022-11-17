cb97753e9853e210febe614d116edd0c319bf15b
Piotr Domagalski <piotr.domagalski@yahoo.com>
2022-11-17 19:03:35 +0100

    cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import meal from '../assets/meals.jpg'
 import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
 
 export default function Header(props) {
   return (
     <>
       <header className={classes.header}>
         <h1>Fake Restaurant</h1>
-        <button>Cart</button>
+        <HeaderCartButton/>
       </header>
       <div className={classes['main-image']}>
         <img src={meal} alt='niestety'/>
