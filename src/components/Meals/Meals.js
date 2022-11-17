ca4d2a875798f823348090acd9a5ce2008eacfa8
Piotr Domagalski <piotr.domagalski@yahoo.com>
2022-11-17 19:03:35 +0100

    form added for adding to cart

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AvalibleMeals from './AvalibleMeals';
+import MealsSummary from './MealsSummary';
+
+export default function Meals() {
+  return (
+    <>
+      <MealsSummary />
+      <AvalibleMeals />
+    </>
+  );
+}
